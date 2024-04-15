e5d2f9415c21e1f0f3c673b2a9766c9bd244739e
Milocco Valentin <valentin-milocco@hotmail.com>
2024-04-15 16:09:06 -0300

    [Parte 3.2], agregue vistas y estilo

--- misitio/polls/views.py
+++ misitio/polls/views.py
@@ -6,26 +6,39 @@ from django.http import HttpResponse
 
 from .models import Question
 
-from django.shortcuts import render
+from django.http import Http404
 
-#def index(request):
-#    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#    context = {"latest_question_list": latest_question_list}
-#    return render(request, "polls/index.html", context)
+from django.shortcuts import get_object_or_404, render
 
+def detail_alt(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, question_id):
+    # Intenta obtener la pregunta con el ID proporcionado
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        # Si la pregunta no existe, lanza una excepción Http404
+        raise Http404("La pregunta no existe")
+    
+    # Si se obtiene la pregunta correctamente, renderiza la plantilla detail.html
+    return render(request, "polls/detail.html", {"question": question})
+
+
+def index_template(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
     return HttpResponse(template.render(context, request))
-
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
 
 
 def results(request, question_id):
