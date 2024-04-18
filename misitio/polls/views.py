64a6061446cc909684949dcb027e9501c3e202fa
Milocco Valentin <valentin-milocco@hotmail.com>
2024-04-17 21:44:34 -0300

    [Parte 4.4], reorganizacion de codigo uso caracteristicas de django de forma mas eficiente

--- misitio/polls/views.py
+++ misitio/polls/views.py
@@ -1,50 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
+from django.views import generic
 
-from django.template import loader
-# Create your views here.
-from django.http import HttpResponse
+from .models import Choice, Question
 
-from .models import Question
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
 
-from django.http import Http404
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by("-pub_date")[:5]
 
-from django.shortcuts import get_object_or_404, render
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def detail_alt(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
-
-
-def detail(request, question_id):
-    # Intenta obtener la pregunta con el ID proporcionado
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        # Si la pregunta no existe, lanza una excepción Http404
-        raise Http404("La pregunta no existe")
-    
-    # Si se obtiene la pregunta correctamente, renderiza la plantilla detail.html
-    return render(request, "polls/detail.html", {"question": question})
-
-
-def index_template(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {"question": question, "error_message": "No seleccionaste ninguna opcion."},
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
